706bc114a251ac27112f785b746c4e80382a8f83
liuboringbook <1287128847@qq.com>
2021-01-31 00:52:57 +0800

    首页

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <main class="content-container" ref="container" :class="headFixed == true ? 'marginTop' : ''">
+      <nuxt/>
+    </main>
+    <v-footer></v-footer>
+  </div>
+</template>
+<script>
+import VHeader from '~/components/home/header'
+import VFooter from '~/components/home/footer'
+
+export default {
+   components:{
+     VHeader,
+     VFooter
+   }
+}
+</script>
+<style>
+  .marginTop{
+    margin-top: 170px;
+  }
+</style>
